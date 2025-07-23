2960124358019c2bbf760f50260b3638472e0e6c
AlpiGun5 <a.i.guncan02@gmail.com>
2025-07-23 18:04:46 +0200

    Init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <fmt/core.h>
+#include <spdlog/spdlog.h>
+
+int main()
+{
+    // Print using fmt
+    fmt::print("Hello, {}!\n", "world");
+    // Print using spdlog
+    spdlog::info("This is an info message from spdlog.");
+    spdlog::warn("This is a warning.");
+    spdlog::error("This is an error message.");
+    // New C++23 feature (optional): deducing this
+    auto greet = [](std::string_view name)
+    {
+        return fmt::format("Hi, {}! Welcome to C++23 🎉", name);
+    };
+    fmt::print("{}\n", greet("Munchkill"));
+    return 0;
+}
